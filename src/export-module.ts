d11beec2f04faa021fabda9bd0542bdad0866490
chen-yang <wszgrcy@gmail.com>
2021-06-01 14:28:16 +0800

    fix: 插件修复导出

--- src/export-module.ts
+++ src/export-module.ts
@@ -1,4 +1,4 @@
 /** 添加要被使用的依赖到此 导出的路径可被子项目直接引用,不支持间接导出即export的文件内存在export,然后使用文件内的export路径,不使用导出共享主项目的命名情况下,请移除所有相关 */
 export * from './service/toast.service';
 export * from './interface/desktop.interface';
-export * from './const/toast.token'
+
